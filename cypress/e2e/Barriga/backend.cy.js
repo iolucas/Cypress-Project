b6314c15b392484f94c992d556088e4bab77e463
Lucas <lucasantunes11@unesc.net>
2023-12-13 16:53:15 -0300

    Update backend

--- cypress/e2e/Barriga/backend.cy.js
+++ cypress/e2e/Barriga/backend.cy.js
@@ -6,14 +6,26 @@ describe("Should test at a functional level", () => {
 
   it("Should create an account", () => {
     cy.request({
-        method: 'POST',
-        url: 'https://barrigarest.wcaquino.me/signin',
-        body: {
-            email: "yonore2792@newcupon.com",
-            redirecionar: false,
-            senha: "96523345"
-        }
-    }).its('body.token').should('not.be.empty')
+      method: "POST",
+      url: "https://barrigarest.wcaquino.me/signin",
+      body: {
+        email: "yonore2792@newcupon.com",
+        redirecionar: false,
+        senha: "96523345",
+      },
+    }).its("body.token").should("not.be.empty")
+      .then((token) => {
+        cy.request({
+          url: "https://barrigarest.wcaquino.me/contas",
+          method: "POST",
+          headers: {
+            Authorization: `JWT ${token}`
+          },
+          body: {
+            nome: "Conta via APIrest",
+          },
+        }).then((res) => console.log(res));
+      });
   });
 
   it("Should update an account", () => {});
